4beed81d9477f7edeed891586ccbf82f07712cb0
aittamaDikra <dikraa.aittama@gmail.com>
2022-01-17 20:36:54 +0100

    make it better

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
   });
-
+app.use(express.json())
 const blocRouter = require('./routes/blocs')
 app.use('/blocs',blocRouter)
 const salleRouter = require('./routes/salles')
